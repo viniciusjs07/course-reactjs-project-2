86686972bcf6af5e24aeda1391a104497a485d04
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2024-06-11 15:09:30 -0300

    example useMemo with useState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useCallback, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+
 import P from 'prop-types';
 
-const Button = React.memo(function Button({ incrementCounter }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => incrementCounter(100)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('filho renderiza');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementCounter: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }).isRequired,
 };
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const classeReverse = 'reverse';
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((number) => {
-    setCounter((c) => c + number);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((res) => setPosts(res));
+    }, 5000);
   }, []);
 
-  const handlerReverse = () => {
-    setReverse(!reverse);
-  };
+  const PostsMemo = useMemo(() => {
+    return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
+  }, [posts]);
 
-  console.log('Pai, renderizou');
-
+  console.log('Pai renderiza');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverse ? classeReverse : ''}`} alt="logo" />
-        <h1>Counter: {counter}</h1>
-        <button onClick={handlerReverse}>Reverse</button>
-        <Button incrementCounter={incrementCounter}></Button>
-      </header>
+      <input type="search" value={value} onChange={(e) => setValue(e.target.value)}></input>
+      {PostsMemo}
+      {posts.length <= 0 && <p>Carregando...</p>}
     </div>
   );
 }
