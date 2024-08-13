77b7d58a0347eccaf524aef608bfc3ebebfaab67
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2024-08-13 12:19:08 -0300

    example useReducer in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
-import React from 'react';
-import { AppContext } from './context/App';
-import { Div } from './components/Div';
+import React, { useReducer } from 'react';
+import { globalState } from './context/App/data';
+
+// o Hook #useReducer deve ser utilizado como manipulação de estados mais complexos
+// que requer alguma lógica.
+const reducer = (state, action) => {
+  console.log('state', state);
+  console.log('action', action);
+  switch (action.type) {
+    case 'change': {
+      console.log('change state type');
+      return { ...state, title: 'Change title' };
+    }
+    case 'invert': {
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <h2>{body}</h2>
+      <button onClick={() => dispatch({ type: 'change' })}>Click</button>
+      <button onClick={() => dispatch({ type: 'invert' })}>Invert</button>
+    </div>
   );
 }
 
